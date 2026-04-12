2d58e0a792ec48c088d6c77be6c5ada111d860ef
DIEGO.erazo <diegoerazo200@gmail.com>
2026-04-11 18:56:37 -0600

    Parcial 02 Fundamentos de programación, N°1 y limpieza de extremos con strip y conteo de caracteres con count

---
+++ ParcialN°2/EjercicioN°1.py
@@ -0,0 +1,5 @@
+animal = "  elefante  "
+animalGrande = animal.strip()
+conteoL = animalGrande.count("e")
+print(f"Texto limpio: '{animalGrande}'")
+print(f"la letra 'e' se repite {conteoL} veces ")
