781095a8849cea2300b35bf0d7918dd2212faa5d
DIEGO.erazo <diegoerazo200@gmail.com>
2026-04-11 19:10:57 -0600

    Parcial 02 Fundamentos de programación, N°9 y sustitución de cadenas con replace y transformación a mayúsculas con upper

---
+++ ParcialN°2/EjercicioN°9.py
@@ -0,0 +1,6 @@
+time = "Any time y Anytime"
+Resultado1 = time.replace("Any time", "Always")
+print("El resultado antes de cambiarlo a mayusculas es:", Resultado1)
+
+Resultado2 = Resultado1.upper()
+print("El resultado despuesde cambiarlo a mayusculas es:", Resultado2)
