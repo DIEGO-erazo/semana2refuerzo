09e3c915d9c73935e932d8919aec70b1e172ea2f
DIEGO.erazo <diegoerazo200@gmail.com>
2026-04-11 19:12:16 -0600

    Parcial 02 Fundamentos de programación, N°11 y combinación de métodos strip, title y centrado de texto con center

---
+++ ParcialN°2/EjercicioN°11.py
@@ -0,0 +1,9 @@
+frase = "  el nido matinal  "
+
+frase_limpia = frase.strip().title()
+
+
+resultado_final = frase_limpia.center(30, "-")
+
+print("Texto procesado:", frase_limpia)
+print("Resultado final:", resultado_final)
