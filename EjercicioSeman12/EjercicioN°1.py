eddd5c20664c9c44c375c936b8ceef616573f550
DIEGO.erazo <diegoerazo200@gmail.com>
2026-04-18 20:52:55 -0600

    Agregar validación de números positivos, negativos y cero

---
+++ EjercicioSeman12/EjercicioN°1.py
@@ -0,0 +1,13 @@
+numero = int(input("Ingrese un número: "))
+
+
+def es_par(numero):
+    if numero >= 1:
+        print("El número es positivo.")
+    elif numero <= -1:
+        print("El número es negativo.")
+    else:
+        print("su numero es cero")
+
+
+es_par(numero)
