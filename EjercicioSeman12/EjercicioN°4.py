4486262d07ddd58f29de95b14101326668bfd670
DIEGO.erazo <diegoerazo200@gmail.com>
2026-04-18 20:56:39 -0600

    Agregar función para determinar números pares e impares

---
+++ EjercicioSeman12/EjercicioN°4.py
@@ -0,0 +1,11 @@
+NUmero = int(input("Ingrese un numero: "))
+
+
+def es_par(numero):
+    if numero % 2 == 0:
+        print("El numero es par")
+    else:
+        print("El numero es impar")
+
+
+es_par(NUmero)
